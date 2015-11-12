dac555f3be6ab9808cac2f7352011a84ac036034
Rohan Jansen <rohan@yoco.co.za>
2015-11-12 17:17:57 +0200

    First commit

---
+++ Dudlz/DudlzTests/DudlzTests.swift
@@ -0,0 +1,36 @@
+//
+//  DudlzTests.swift
+//  DudlzTests
+//
+//  Created by Rohan Jansen on 2015/11/12.
+//  Copyright © 2015 Rohan Jansen. All rights reserved.
+//
+
+import XCTest
+@testable import Dudlz
+
+class DudlzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
